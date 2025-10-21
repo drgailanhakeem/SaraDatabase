7438f92619d9aaab98979d6bfc651e3ec323260b
drgailanhakeem <dr.gailan.hakeem@gmail.com>
2025-10-21 16:39:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 import gspread
+from google.oauth2 import service_account
 from google.oauth2.service_account import Credentials
 
 st.title("My Google Sheet Data")
